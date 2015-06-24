db12d05b0c4cb8b356ff16d966da919cb2f98f77
amitparmar01 <parmar.amit@gmail.com>
2015-06-24 14:28:44 +0530

    Update list-common.py

--- practicepython/list-common.py
+++ practicepython/list-common.py
@@ -1,18 +1,35 @@
 
+import random 
+
+def common_list(a, b):
+    #a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 2, 1, 3, 4, 00]
+    #b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    common = []
+    big = []
+    small = []
+
+    if len(a) > len(b):
+        big  = a
+        small = b
+    else:
+        big = b
+        small = a
+
+    for item in big:
+        if item in small and not item in common:
+            common.append(item)
+
+    return sorted(common)
+
 def main():
-	
-	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-  	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-  	c =[]
 
-  	for item in b:
-  		if item in a:
-  			c.append(a)
+    lengthA = random.randint(1, 20)
+    lengthB = random.randint(1, 20)
 
-  	print (c)
+    a = random.sample(range(1, 101), lengthA)
+    b = random.sample(range(1, 101), lengthB)
 
-
-
+    print(common_list(a,b))
 
 if __name__ == "__main__":
 	main()
